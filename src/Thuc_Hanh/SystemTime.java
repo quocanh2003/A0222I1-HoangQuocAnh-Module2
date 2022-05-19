c357dfbabdcfab22f0ed0c678319ff53b8d39a93
quocanh2003 <100422286+quocanh2003@users.noreply.github.com>
2022-05-19 21:53:06 +0700

    up

---
+++ src/Thuc_Hanh/SystemTime.java
@@ -0,0 +1,11 @@
+package Thuc_Hanh;
+
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("now is: "+ now);
+    }
+}
