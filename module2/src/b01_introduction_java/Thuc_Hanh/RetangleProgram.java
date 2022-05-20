a900a6bd3d776d7b9d971c6a1e53766e0709690c
quocanh2003 <100422286+quocanh2003@users.noreply.github.com>
2022-05-20 18:39:17 +0700

    up

---
+++ module2/src/b01_introduction_java/Thuc_Hanh/RetangleProgram.java
@@ -0,0 +1,20 @@
+package b01_introduction_java.Thuc_Hanh;
+
+import java.util.Scanner;
+
+public class RetangleProgram {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+
+        float area = width * height;
+        System.out.println("Retangle Area is: " +area);
+    }
+
+}
---
+++ module2/src/b01_introduction_java/Thuc_Hanh/SystemTime.java
@@ -0,0 +1,10 @@
+package b01_introduction_java.Thuc_Hanh;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("now is: "+ now);
+    }
+}
