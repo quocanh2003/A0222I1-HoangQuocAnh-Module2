0aa669869e908a8b679c4932011d5dd7d1ad6be4
quocanh2003 <100422286+quocanh2003@users.noreply.github.com>
2022-06-17 21:54:08 +0700

    up

---
+++ module2/src/ss10_Stack_Queue/Bai_tap/queue/Node.java
@@ -0,0 +1,11 @@
+package ss10_Stack_Queue.Bai_tap.queue;
+
+public class Node {
+    public int data;
+    public Node link;
+
+    public Node(int data) {
+        this.data = data;
+        this.link = null;
+    }
+}
---
+++ module2/src/ss11_Java_Collection_Framework/Bai_tap/democollection/repository/ProductRepository.java
@@ -0,0 +1,68 @@
+package ss11_Java_Collection_Framework.Bai_tap.democollection.repository;
+
+import ss11_Java_Collection_Framework.Bai_tap.democollection.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository {
+    private static List<Product> productList;
+    static {
+        productList = new ArrayList<>();
+    }
+
+    public void addProduct(Product p) {
+        productList.add(p);
+    }
+
+    public boolean editProduct(Product p) {
+        for (Product product: productList) {
+            if(product.getId() == p.getId()) {
+                product.setName(p.getName());
+                product.setPrice(p.getPrice());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteProduct(int id) {
+        Product p = null;
+        for (Product product: productList) {
+            if (product.getId() == id) {
+                p = product;
+            }
+        }
+        if (p != null) {
+            productList.remove(p);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public Product getProductById(int id) {
+        for (Product product: productList) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Product> findProductByName(String name) {
+        List<Product> list = new ArrayList<>();
+        for (Product product: productList) {
+            if (product.getName().contains(name)) {
+                list.add(product);
+            }
+        }
+
+        return list;
+    }
+
+}
