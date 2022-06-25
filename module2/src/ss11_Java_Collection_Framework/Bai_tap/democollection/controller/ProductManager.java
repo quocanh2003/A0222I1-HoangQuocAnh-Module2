0d552a5d12f1030d1f73c37b7525dd733035705e
quocanh2003 <100422286+quocanh2003@users.noreply.github.com>
2022-06-25 17:40:45 +0700

    up

--- module2/src/ss11_Java_Collection_Framework/Bai_tap/democollection/controller/ProductManager.java
+++ module2/src/ss11_Java_Collection_Framework/Bai_tap/democollection/controller/ProductManager.java
@@ -1,14 +1,13 @@
 package Bai_tap.democollection.controller;
 
-import ss11_Java_Collection_Framework.Bai_tap.democollection.entity.Product;
-import ss11_Java_Collection_Framework.Bai_tap.democollection.service.ProductService;
+import Bai_tap.democollection.entity.Product;
+import Bai_tap.democollection.service.ProductService;
 
 import java.util.List;
 
 public class ProductManager {
     public static void main(String[] args) {
         ProductService service = new ProductService();
-
         Product p1 = new Product(1, "Giay", 230000);
         Product p2 = new Product(2, "Ao", 220000);
         Product p3 = new Product(3, "Quan", 210000);
@@ -25,6 +24,7 @@ public class ProductManager {
 
         List<Product> products = service.getProductList();
         service.sortByPriceDesc(products);
-        products.stream().forEach(p -> System.out.println(p) );
+        products.stream().forEach(p -> System.out.println(p));
     }
 }
+
--- module2/src/ss11_Java_Collection_Framework/Bai_tap/democollection/repository/ProductRepository.java
+++ module2/src/ss11_Java_Collection_Framework/Bai_tap/democollection/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package Bai_tap.democollection.repository;
 
-import ss11_Java_Collection_Framework.Bai_tap.democollection.entity.Product;
+import Bai_tap.democollection.entity.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,59 +10,58 @@ public class ProductRepository {
     static {
         productList = new ArrayList<>();
     }
-
-    public void addProduct(Product p) {
+    public void addProduct(Product p){
         productList.add(p);
     }
-
-    public boolean editProduct(Product p) {
-        for (Product product: productList) {
-            if(product.getId() == p.getId()) {
+    public boolean editProduct(Product p){
+        for (Product product: productList){
+            if (product.getId() == p.getId()){
                 product.setName(p.getName());
                 product.setPrice(p.getPrice());
-                return true;
+                return true
             }
         }
         return false;
     }
-
-    public boolean deleteProduct(int id) {
+    public boolean deleteProduct(int id){
         Product p = null;
-        for (Product product: productList) {
-            if (product.getId() == id) {
+        for (Product product: productList){
+            if (product.getId()== id){
                 p = product;
             }
         }
-        if (p != null) {
+        if (p != null){
             productList.remove(p);
             return true;
         }
         return false;
     }
-
-    public List<Product> getProductList() {
+    public List<Product> getProductList(){
+        return productList;
+    }
+    public Product getProductList() {
         return productList;
     }
 
-    public Product getProductById(int id) {
-        for (Product product: productList) {
-            if (product.getId() == id) {
-                return product;
+        public Product getProductById ( int id){
+            for (Product product : productList) {
+                if (product.getId() == id) {
+                    return product;
+                }
             }
+
+            return null;
         }
 
-        return null;
-    }
-
-    public List<Product> findProductByName(String name) {
-        List<Product> list = new ArrayList<>();
-        for (Product product: productList) {
-            if (product.getName().contains(name)) {
-                list.add(product);
+        public List<Product> findProductByName (String name){
+            List<Product> list = new ArrayList<>();
+            for (Product product : productList) {
+                if (product.getName().contains(name)) {
+                    list.add(product);
+                }
             }
+
+            return list;
         }
-
-        return list;
     }
-
 }
